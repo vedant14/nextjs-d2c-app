12db9ba1c02e1320e3db232abae10b0e1f67fa75
Vedant Lohbare <vedant.lohbare@gmail.com>
2023-01-15 23:36:30 +0530

    Testing the scripts

--- web/public/test-script.js
+++ web/public/test-script.js
@@ -1,1 +1,12 @@
-console.log("hiii");
+console.log("HEY");
+const script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+script.type = "text/javascript";
+script.onreadystatechange = handler;
+script.onload = handler;
+document.getElementsByTagName("head")[0].appendChild(script);
+function handler() {
+  const body = $("body");
+  const shop = Shopify.shop;
+  console.log(shop);
+}
